b2d249fcb1674adba0d6e69b0f8f7cc32f36a9dd
Pearce Keesling <pearce.keesling@lifeomic.com>
2019-10-17 12:25:22 -0600

    creating quad pipeline

---
+++ src/render/font.rs
@@ -0,0 +1,39 @@
+use wgpu::TextureView;
+use wgpu_glyph::{Section};
+use winit::dpi::PhysicalSize;
+
+pub struct Font {
+    glyphs: wgpu_glyph::GlyphBrush<'static, ()>,
+}
+
+impl Font {
+    pub fn from_bytes(device: &mut wgpu::Device, bytes: &'static [u8]) -> Font {
+        Font {
+            glyphs: wgpu_glyph::GlyphBrushBuilder::using_font_bytes(bytes)
+                .texture_filter_method(wgpu::FilterMode::Nearest)
+                .build(device, wgpu::TextureFormat::Bgra8UnormSrgb),
+        }
+    }
+
+    pub fn queue(&mut self, section: Section<'_>) {
+        self.glyphs.queue(section);
+    }
+
+    pub fn draw(
+        &mut self,
+        device: &mut wgpu::Device,
+        encoder: &mut wgpu::CommandEncoder,
+        target: &TextureView,
+        size: &PhysicalSize,
+    ) {
+        self.glyphs
+            .draw_queued(
+                device,
+                encoder,
+                target,
+                size.width.round() as u32,
+                size.height.round() as u32,
+            )
+            .expect("Draw font");
+    }
+}
---
+++ src/render/mod.rs
@@ -0,0 +1,126 @@
+use wgpu::{CommandEncoder, Device, Surface, SwapChain, SwapChainOutput, Queue, RenderPipeline, BindGroup, Buffer};
+use wgpu_glyph::{Section};
+use winit::{
+    dpi::PhysicalSize,
+    window::Window,
+};
+
+
+mod font;
+mod quad;
+
+use font::Font;
+
+pub struct Renderer {
+    font: Font,
+    swap_chain: SwapChain,
+    queue: Queue,
+    device: Device,
+    surface: Surface,
+}
+
+const RENDER_FORMAT: wgpu::TextureFormat = wgpu::TextureFormat::Bgra8UnormSrgb;
+
+impl Renderer {
+    pub fn on_window(window: &Window) -> Self {
+        let adapter = wgpu::Adapter::request(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::HighPerformance,
+            backends: wgpu::BackendBit::all(),
+        })
+        .expect("Request adapter");
+
+        let (mut device, mut queue) = adapter.request_device(&wgpu::DeviceDescriptor {
+            extensions: wgpu::Extensions {
+                anisotropic_filtering: false,
+            },
+            limits: wgpu::Limits { max_bind_groups: 1 },
+        });
+
+        let surface = wgpu::Surface::create(window);
+        let inconsolata: &[u8] = include_bytes!("FiraMono-Regular.ttf");
+        let font = Font::from_bytes(&mut device, &inconsolata);
+
+        let size = window.inner_size().to_physical(window.hidpi_factor());
+        let swap_chain = device.create_swap_chain(
+            &surface,
+            &wgpu::SwapChainDescriptor {
+                usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
+                format: RENDER_FORMAT,
+                width: size.width.round() as u32,
+                height: size.height.round() as u32,
+                present_mode: wgpu::PresentMode::Vsync,
+            },
+        );
+        Renderer {
+            font,
+            swap_chain,
+            queue,
+            device,
+            surface,
+        }
+    }
+
+    pub fn update_size(&mut self, size: PhysicalSize) {
+        self.swap_chain = self.device.create_swap_chain(
+            &self.surface,
+            &wgpu::SwapChainDescriptor {
+                usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
+                format: RENDER_FORMAT,
+                width: size.width.round() as u32,
+                height: size.height.round() as u32,
+                present_mode: wgpu::PresentMode::Vsync,
+            },
+        );
+    }
+
+    pub fn start_frame<'a>(&'a mut self) -> RenderFrame<'a> {
+        let frame: SwapChainOutput<'a> = self.swap_chain.get_next_texture();
+        let encoder =
+            self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                todo: 0,
+            });
+        RenderFrame {
+            frame,
+            font: &mut self.font,
+            queue: &mut self.queue,
+            device: &mut self.device,
+            encoder,
+        }
+    }
+}
+
+pub struct RenderFrame<'a> {
+    frame: SwapChainOutput<'a>,
+    font: &'a mut Font,
+    queue: &'a mut Queue,
+    device: &'a mut Device,
+    encoder: CommandEncoder,
+}
+
+impl<'a> RenderFrame<'a> {
+    pub fn clear(&mut self) {
+        let _ = self.encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
+                attachment: &self.frame.view,
+                resolve_target: None,
+                load_op: wgpu::LoadOp::Clear,
+                store_op: wgpu::StoreOp::Store,
+                clear_color: wgpu::Color {
+                    r: 0.,
+                    g: 0.169,
+                    b: 0.2117,
+                    a: 1.0,
+                },
+            }],
+            depth_stencil_attachment: None,
+        });
+    }
+    pub fn queue_text(&mut self, section: Section) {
+        self.font.queue(section);
+    }
+
+    pub fn submit(mut self, size: &PhysicalSize) {
+        self.font.draw(self.device, &mut self.encoder, &self.frame.view, size);
+        self.queue.submit(&[self.encoder.finish()]);
+    }
+}
